a47aa5417e6c94e58bdc81c7cc17e6f967d76868
Zhao-666 <2268699370@qq.com>
2019-01-24 22:42:53 +0800

    create project

---
+++ controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 2019/1/24
+ * Time: 22:37
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class IndexController extends Controller
+{
+    public function actionIndex()
+    {
+        echo "HelloWorld!!!";
+    }
+}
